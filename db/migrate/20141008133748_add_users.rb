194f4356fbf8e000d62f2c9ee3df381aa9d6db7a
Supakorn Laohasongkram & Daniel Schlessinger & John Lin <pair+klaoha06.dschlessinger.johnlin1214@devbootcamp.com>
2014-10-10 00:39:24 -0700

    style changes

--- db/migrate/20141008133748_add_users.rb
+++ db/migrate/20141008133748_add_users.rb
@@ -1,8 +1,15 @@
 class AddUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
       t.string :username
       t.string :password_hash
+      t.string :email
+      t.string :city
+      t.string :state
+      t.string :gender
+
       t.timestamps
     end
   end
